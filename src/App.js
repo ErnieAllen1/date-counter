300de6cef30e21227e2e992385ace8d71167c037
ErnieAllen1 <ernestmessina@gmail.com>
2025-09-04 12:39:48 +0800

    Date editor, based on number of steps and count

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Counter />
     </div>
   );
 }
 
+function Counter() {
+  const [counter, setCounter] = useState(0);
+  const [step, setStep] = useState(1);
+  const date = new Date();
+  date.setDate(date.getDate() + counter);
+  return (
+    <div>
+      <div>
+        <button onClick={() => setStep((s) => (s === 0 ? 0 : s - 1))}>-</button>
+        <span>Steps: {step}</span>
+        <button onClick={() => setStep((s) => s + 1)}>+</button>
+      </div>
+      <div>
+        <button onClick={() => setCounter((c) => c - step)}>-</button>
+        <span>Count: {counter}</span>
+        <button onClick={() => setCounter((c) => c + step)}>+</button>
+      </div>
+      <p>{counter === 0 && `Today is ${date.toDateString()}`}</p>
+      <p>
+        {counter < 0 &&
+          `${Math.abs(counter)} days ago it was ${date.toDateString()}`}
+      </p>
+      <p>
+        {counter > 0 &&
+          `In ${counter} days time it will be ${date.toDateString()}`}
+      </p>
+    </div>
+  );
+}
 export default App;
